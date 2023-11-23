4ddfc3a8d732311c58b65e592575e495252bb07b
Cristian Alaniz Benitez <alaniz21cris@gmail.com>
2023-11-22 19:05:32 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+const num1 = 31;
+const num2 = 2;
 
+const multiply = num1 * num2;
+
+let random = Math.ceil(Math.random());
+
+const num3 = 36;
+const num4 = 9;
+
+const mod = num3 / num4;
+
+const max = Math.max(2, 3, 5, 20);
 // Write your code here
